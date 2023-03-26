44510e06f1dc5624af87fe7f5dbd14baf0e98bbb
Yogesh Bisht <yogesh.bisht@iotfy.co>
2023-03-26 17:01:31 +0530

    request routing handing and dependencies middleware added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
-from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import recipes
 
-app = FastAPI()
 
-app.include_router(recipes.router)
+# v1 API
+apiv1 = FastAPI()
+apiv1.include_router(recipes.router, prefix="/recipes", tags=["recipes"])
+
+
+
+app = FastAPI()
+app.mount("/api/v1", apiv1)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- app/routers/recipes.py
+++ app/routers/recipes.py
@@ -1,44 +1,58 @@
 import uuid
 from typing import Annotated, Union, List
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, Depends, Request
 from app.models.request.recipes import RecipeIn
 from app.models.response.recipes import RecipeOut
+from app.dependencies import valid_api_key_required
 
-router = APIRouter(prefix="/recipes", tags=["recipes"])
+
+router = APIRouter(dependencies=[Depends(valid_api_key_required)])
 
 
 @router.get("/")
-async def get_recipes(X_HONEY_FOOD_API_KEY: Annotated[Union[str, None], Header()]) -> List[RecipeOut]:
-    print(X_HONEY_FOOD_API_KEY)
-    return [{"id": str(uuid.uuid4())}, {"id": str(uuid.uuid4())}]
+async def get_recipes(request: Request) -> List[RecipeOut]:
+    return [
+        RecipeOut(**{
+            "id": str(uuid.uuid4()),
+            "title": "stringstri",
+            "slug": "stringstri",
+            "description": "stringstringstringst",
+            "content": "stringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringst",
+            "image_type": "application/octet-stream",
+            "published": True,
+            "created": 0,
+            "updated": 0
+       }),
+       RecipeOut(**{
+            "id": str(uuid.uuid4()),
+            "title": "stringstri",
+            "slug": "stringstri",
+            "description": "stringstringstringst",
+            "content": "stringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringst",
+            "image_type": "application/octet-stream",
+            "published": True,
+            "created": 0,
+            "updated": 0
+        })
+    ]
 
 
 @router.post("/")
-async def add_recipe(
-    recipe: RecipeIn,
-    X_HONEY_FOOD_API_KEY: Annotated[Union[str, None], Header()]
-) -> RecipeOut:
-    print(X_HONEY_FOOD_API_KEY)
-    return {"id": str(uuid.uuid4()), **recipe.dict()}
+async def add_recipe(request: Request, recipe: RecipeIn) -> RecipeOut:
+    print(request)
+    return {"id": str(uuid.uuid4())}
 
 
 @router.get("/{id}")
-async def get_recipe(
-    id: str,
-    X_HONEY_FOOD_API_KEY: Annotated[Union[str, None], Header()]
-) -> RecipeOut:
+async def get_recipe(id: str, request: Request) -> RecipeOut:
     return {"id": id}
 
 
 @router.put("/{id}")
-async def update_recipe(
-    id: str,
-    recipe: RecipeIn,
-    X_HONEY_FOOD_API_KEY: Annotated[Union[str, None], Header()]
-) -> RecipeOut:
+async def update_recipe(id: str, request: Request, recipe: RecipeIn) -> RecipeOut:
     return {"id": id}
 
 
 @router.delete("/{id}")
-async def delete_recipe(id: str) -> str:
+async def delete_recipe(id: str, request: Request,) -> str:
     return {"id": id}
